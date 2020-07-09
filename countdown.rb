0f54177397364fcaa30ee72147227a20ec4c8d97
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-09 03:11:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+def countdown (number)
+ while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+ end
+ "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep
+  runtime = Benchmark.measure {countdown_with_sleep(5)}
+    puts runtime <= 5 
 end
